8c3c9d3e1e81a2558829b0c93ea20d55ce27a89f
Undo1 <parker@erwaysoftware.com>
2013-11-25 01:39:12 +0000

    added SE API key

--- context.php
+++ context.php
@@ -3,7 +3,7 @@
 include "base.php";
  
 $url = 'https://api.stackexchange.com/2.1/posts/' . $_POST["id"] . '/comments';
-$data = array("site" => $_SESSION["Site"], "filter" => "!SrhZo6aE2O(w*j4-4i", "order" => "asc");
+$data = array("site" => $_SESSION["Site"], "filter" => "!SrhZo6aE2O(w*j4-4i", "order" => "asc", key => "mmpZxopkL*psP5WoBK6BuA((");
  
 $response = (new Curl)->exec($url . '?' . http_build_query($data), [CURLOPT_ENCODING => 'gzip']);
  
