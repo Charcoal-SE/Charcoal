c3fb3979a9bd6132b15e3e37edaab1f5981cbeed
Undo1 <parker@erwaysoftware.com>
2013-11-25 01:27:27 +0000

    reversed order of comment context so that they're displayed oldest-first

--- context.php
+++ context.php
@@ -3,7 +3,7 @@
 include "base.php";
  
 $url = 'https://api.stackexchange.com/2.1/posts/' . $_POST["id"] . '/comments';
-$data = array("site" => $_SESSION["Site"], "filter" => "!SrhZo6aE2O(w*j4-4i");
+$data = array("site" => $_SESSION["Site"], "filter" => "!SrhZo6aE2O(w*j4-4i", "order" => "asc");
  
 $response = (new Curl)->exec($url . '?' . http_build_query($data), [CURLOPT_ENCODING => 'gzip']);
  
