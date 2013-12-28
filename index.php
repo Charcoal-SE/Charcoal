29276b32026aa07478ac99678460abaa65b7cc40
Manish Goregaokar <Manishearth@users.noreply.github.com>
2013-12-28 23:24:47 +0530

    unicode

--- index.php
+++ index.php
@@ -103,7 +103,7 @@
 </div>	
 <div id="collectmodal" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true" style="display: none;"><div class="modal-dialog"><div class="modal-content">
 		<div class="modal-header">
-		<button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+		<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&#215;</button>
 		<h3 id="myModalLabel">Comment collection system</h3>
 		</div>
 		<div class="modal-body">
