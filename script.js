a78449ad0e62ffe94b55a4434b30354405b2e542
Manish Goregaokar <Manishearth@users.noreply.github.com>
2013-12-28 21:00:57 +0530

    errant apostrophe

--- script.js
+++ script.js
@@ -62,7 +62,7 @@ if(taskList.length>0){
 	console.log("Purging post #"+(originallength - taskList.length+1) +" of "+  originallength)
 	var postid=taskList.shift()
 	$.post("/admin/posts/"+postid+"/delete-comments",
-		JSON.parse('{"fkey":"'+StackExchange.options.user.fkey+'","target-post-id":'+postid+'',"mod-actions":"delete-comments","duration":1}'),
+		JSON.parse('{"fkey":"'+StackExchange.options.user.fkey+'","target-post-id":'+postid+',"mod-actions":"delete-comments","duration":1}'),
 		function(){console.log('(done)');setTimeout(POSTFlag,500);}
 	);
 }else{
