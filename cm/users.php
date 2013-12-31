92f739d02aa1f89c0ae0279a12c5b62e02acf8c9
Undo1 <Undo1@users.noreply.github.com>
2013-12-31 15:56:25 -0700

    just an idea

--- cm/users.php
+++ cm/users.php
@@ -69,7 +69,7 @@
             while ($row1 = mysql_fetch_array($sites))
              {
                 $aQuery = mysql_query("SELECT COUNT(*) AS number FROM " . $row1["siteTableName"] . " WHERE handled=1 
-                 AND handledBy = " . $row["id"] . "");
+                 AND handledBy=" . $row["id"]);
                  $handled = mysql_fetch_assoc($aQuery);
                  $numhandled = $handled["number"];
                  $totalhandled = $numhandled + $totalhandled;
