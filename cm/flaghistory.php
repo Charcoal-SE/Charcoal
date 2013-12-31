7742961f1aa8785367e1c5f74a5503f31f8d49f3
hichris1234 <christopherncarmel@hotmail.com>
2013-12-31 10:37:54 -0500

    Limit number in flag history to 100

--- cm/flaghistory.php
+++ cm/flaghistory.php
@@ -27,7 +27,7 @@
     <div class="col-md-offset-1 col-md-10">
     <table class="table main-table">
         <?php
-            $query = mysql_query("SELECT handleDate, Text, wasValid FROM " . $_SESSION["Site"] . " WHERE handled=1 order by handleDate desc");
+            $query = mysql_query("SELECT handleDate, Text, wasValid FROM " . $_SESSION["Site"] . " WHERE handled=1 order by handleDate desc LIMIT 100");
             while ($row = mysql_fetch_array($query))
             {
             echo "<tr class='date-row' id='" . $row['handleDate'] . "'><td>";
