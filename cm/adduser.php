f74ff48462dcab645b84b8f1bc352051ace18ebe
Undo1 <Undo1@users.noreply.github.com>
2014-01-05 15:51:44 -0700

    Update adduser.php

--- cm/adduser.php
+++ cm/adduser.php
@@ -1,2 +1,9 @@
 <?php
-
+  if(!empty($_SESSION['LoggedIn']) && !empty($_SESSION['Username']) && $_SESSION["ischarcoalmod"]==1)  
+  {
+    
+  }
+  else
+  {
+    echo 'You\'re not logged in!';
+  }
