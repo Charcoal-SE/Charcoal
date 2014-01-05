f5da38313f486f1011e3841be8df6f93f320894e
Undo1 <parker@erwaysoftware.com>
2014-01-05 16:29:15 -0700

    made adduser.php functional

--- cm/adduser.php
+++ cm/adduser.php
@@ -1,7 +1,19 @@
 <?php
+include '../base.php';
   if(!empty($_SESSION['LoggedIn']) && !empty($_SESSION['Username']) && $_SESSION["ischarcoalmod"]==1)  
   {
-    
+  	$username = $_REQUEST["username"];
+  	$password = $_REQUEST["password"];
+  	$email = $_REQUEST["email"];
+  	$isnetworkmod = $_REQUEST['isnetworkmod'];
+
+  	$db = new PDO('mysql:host=' . MySQLHost() . ';dbname=' . MySQLDB() . ';charset=utf8', MySQLUsername(), MySQLPassword());
+
+	$stmt = $db->prepare("INSERT INTO users(`username`,`password`,`email`,`isnetworkmod`,`isDev`) VALUES(:username,:password,:email,:isnetworkmod,0)");
+	$stmt->execute(array(':username' => $username, ':password' => md5($password), ':email' => $email, ':isnetworkmod' => $isnetworkmod));
+	$affected_rows = $stmt->rowCount();
+
+	echo $affected_rows.' were affected';
   }
   else
   {
