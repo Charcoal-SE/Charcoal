f81e926b181e40d91ee795529385cfe929f502ef
Undo1 <Undo1@users.noreply.github.com>
2014-01-04 19:01:18 -0700

    Update submitFlaggable.php

--- submitFlaggable.php
+++ submitFlaggable.php
@@ -1,5 +1,8 @@
 <?php
         include "base.php";
+        
+        if ($_SESSION['ischarcoalmod']==0) return;
+        
         $timestamp = date("Y:m:d H:i:s");
 
         mysql_query("UPDATE " . $_SESSION["Site"] . " SET handled=1, handleDate='$timestamp', wasValid=1, wasObsolete=1, toFlag=1, handledBy=" . $_SESSION["UserID"] . " WHERE `Id`=" . $_POST['id']);
