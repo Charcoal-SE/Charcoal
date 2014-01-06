23255047812f50ae4f052a5ec06c2e4fb858a34d
hichris1234 <christopherncarmel@hotmail.com>
2014-01-06 12:29:16 -0500

    Update submitValid.php

--- submitValid.php
+++ submitValid.php
@@ -2,7 +2,7 @@
 	include "base.php";
 	$timestamp = date("Y:m:d H:i:s");
 
-	mysql_query("UPDATE " . $_SESSION["Site"] . " SET handled=1, handleDate='$timestamp', wasValid=1, wasObsolete=1, handledBy=" . $_SESSION["UserID"] . " WHERE `Id`=" . $_POST['id']);
+	mysql_query("UPDATE flags SET handled=1, handleDate='$timestamp', wasValid=1, wasObsolete=1, handledBy=" . $_SESSION["UserID"] . " WHERE `Id`=" . $_POST['id']. " AND  `site`='" . $_SESSION["Site"] . "'");
 
 	echo "success";
 ?>
