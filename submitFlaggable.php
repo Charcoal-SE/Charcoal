084a4c63907076b01def6c2e5afa8381d38df61f
hichris1234 <christopherncarmel@hotmail.com>
2014-01-06 12:29:48 -0500

    Update submitFlaggable.php

--- submitFlaggable.php
+++ submitFlaggable.php
@@ -5,7 +5,7 @@
         
         $timestamp = date("Y:m:d H:i:s");
 
-        mysql_query("UPDATE " . $_SESSION["Site"] . " SET handled=1, handleDate='$timestamp', wasValid=1, wasObsolete=1, toFlag=1, handledBy=" . $_SESSION["UserID"] . " WHERE `Id`=" . $_POST['id']);
+        mysql_query("UPDATE flags SET handled=1, handleDate='$timestamp', wasValid=1, wasObsolete=1, toFlag=1, handledBy=" . $_SESSION["UserID"] . " WHERE `Id`=" . $_POST['id']. " AND  `site`='" . $_SESSION["Site"] . "'");
 
         echo "success";
 ?>
