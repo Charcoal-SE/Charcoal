acabf6be332e0381a93a87bc48fad3f09b510a59
hichris1234 <christopherncarmel@hotmail.com>
2014-01-06 20:07:48 -0500

    Update cronjob_2.php

--- cronjob_2.php
+++ cronjob_2.php
@@ -8,10 +8,13 @@
   $done = 0;
   while ($number - $done > 0)
   {
+    $result = array();
     $done = 100 + $done;
     $getflags = mysql_query("SELECT `Id`, `site` FROM flags WHERE handled=0 ORDER BY LENGTH(`Text`) LIMIT 100");
-    $flagid = mysql_fetch_assoc($getflags);
-    $url = 'https://api.stackexchange.com/2.1/posts/' . $flagid["Id"] . '/comments';
+    while ($flagid = mysql_fetch_assoc($getflags){
+      array_push($result, $flagid["Id"]);
+    }
+    $url = 'https://api.stackexchange.com/2.1/posts/' . $result . '/comments';
     $data = array("site" => $flagid["site"], "filter" => "!SrhZo6aE2O(w*j4-4i", "order" => "asc", key => "mmpZxopkL*psP5WoBK6BuA((");
     $response = (new Curl)->exec($url . '?' . http_build_query($data), [CURLOPT_ENCODING => 'gzip']);
   }
