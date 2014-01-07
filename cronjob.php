bb570b2e26fa8d1e63a60a10686f40d4d088cc8c
Undo1 <Undo1@users.noreply.github.com>
2014-01-06 18:05:23 -0700

    Update cronjob.php

--- cronjob.php
+++ cronjob.php
@@ -33,36 +33,6 @@
 
 	print($obj1);
 
-class Curl
-{
-  protected $info = [];
-  
-  public function exec($url, $setopt = array(), $post = array())
-  {
-    $curl = curl_init();
-    curl_setopt($curl, CURLOPT_URL, $url);
-    curl_setopt($curl, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:15.0) Gecko/20100101 Firefox/15.0.1');
-    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-    curl_setopt($curl, CURLOPT_FOLLOWLOCATION, 1);
-    curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, 0);
-    curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, 2);
-    if ( ! empty($post))
-    {
-      curl_setopt($curl, CURLOPT_POST, 1);
-      curl_setopt($curl, CURLOPT_POSTFIELDS, $post);
-    }
-    if ( ! empty($setopt))
-    {
-      foreach ($setopt as $key => $value)
-      {
-        curl_setopt($curl, $key, $value);
-      }
-    }
-    $data = curl_exec($curl);
-    $this->info = curl_getinfo($curl);
-    curl_close($curl);
-    return $data;
-  }
  
   public function getInfo()
   {
