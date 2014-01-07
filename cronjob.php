31dc418243aa72771d2ead1928943e05b3d3a917
Undo1 <Undo1@users.noreply.github.com>
2014-01-06 17:58:23 -0700

    Create cronjob.php

---
+++ cronjob.php
@@ -0,0 +1,70 @@
+<?php
+    
+    include "config.php";
+
+	$dbhost = MySQLHost();
+	$dbname = MySQLDB();
+	$dbuser = MySQLUsername();
+	$dbpass = MySQLPassword();
+  
+	mysql_connect($dbhost, $dbuser, $dbpass) or die("MySQL Error: " . mysql_error());  
+	mysql_select_db($dbname) or die("MySQL Error: " . mysql_error()); 
+
+	$siteArr = mysql_fetch_assoc(mysql_query("select siteRootURL, siteTableName from sites order by lastCronCheck asc limit 1"));
+	$siteAPIKey = $siteArr['siteRootURL'];
+	$siteTableName = $siteArr["siteTableName"];
+
+	$query = mysql_query("SELECT `Id` FROM flags WHERE site='" . $siteTableName . "' AND handled=0 ORDER BY LENGTH(`Text`) limit 100");
+	$commentsToInspect = array();
+
+	while ($row = mysql_fetch_array($query))
+	{
+		$commentsToInspect[] = $row["Id"];
+	}
+
+	$url = 'https://api.stackexchange.com/2.1/comments/' . implode(";", $commentsToInspect);
+	$data = array("site" => $siteAPIKey, "filter" => "!9im-EfY_i", "order" => "asc", 'key' => "mmpZxopkL*psP5WoBK6BuA((");
+ 
+	$response = (new Curl)->exec($url . '?' . http_build_query($data), [CURLOPT_ENCODING => 'gzip']);
+ 
+	$obj1 = json_decode($response);
+	// $obj = $obj1['items'];
+
+	print($obj1);
+
+class Curl
+{
+  protected $info = [];
+  
+  public function exec($url, $setopt = array(), $post = array())
+  {
+    $curl = curl_init();
+    curl_setopt($curl, CURLOPT_URL, $url);
+    curl_setopt($curl, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:15.0) Gecko/20100101 Firefox/15.0.1');
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($curl, CURLOPT_FOLLOWLOCATION, 1);
+    curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, 0);
+    curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, 2);
+    if ( ! empty($post))
+    {
+      curl_setopt($curl, CURLOPT_POST, 1);
+      curl_setopt($curl, CURLOPT_POSTFIELDS, $post);
+    }
+    if ( ! empty($setopt))
+    {
+      foreach ($setopt as $key => $value)
+      {
+        curl_setopt($curl, $key, $value);
+      }
+    }
+    $data = curl_exec($curl);
+    $this->info = curl_getinfo($curl);
+    curl_close($curl);
+    return $data;
+  }
+ 
+  public function getInfo()
+  {
+    return $this->info;
+  }
+}
