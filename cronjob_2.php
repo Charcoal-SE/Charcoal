b7820a21deaf3c0764cec1ba12df598ad10db0be
hichris1234 <christopherncarmel@hotmail.com>
2014-01-07 12:12:54 -0500

    Update cronjob_2.php

--- cronjob_2.php
+++ cronjob_2.php
@@ -1,20 +1,21 @@
 <?php
   //magic goes here
   include "base.php";
-  
+  echo "<span class='small'>Started cronjob</span>";
   $sites = mysql_query("SELECT * FROM sites");
   while ($site = mysql_fetch_array($sites))
   {
-    $query = mysql_query("SELECT Count(*) AS number, FROM flags WHERE handled=0 AND site='" . $site["siteTableName"] . "' ORDER BY LENGTH(`Text`)");
-    $row = mysql_fetch_assoc($query);
-    $number = $row["number"];
-    print_r($number);
-    $done = 0;
-    while ($number - $done > 0)
-    {
+    //$query = mysql_query("SELECT Count(*) AS number, FROM flags WHERE handled=0 AND site='" . $site["siteTableName"] . "' ORDER BY LENGTH(`Text`)");
+    //$row = mysql_fetch_assoc($query);
+    //$number = $row["number"];
+    //print_r($number);
+    //$done = 0;
+    //while ($number - $done > 0)
+    //{
       $result = array();
-      $done = 100 + $done;
-      $getflags = mysql_query("SELECT `Id`, `site` FROM flags WHERE handled=0 AND site='" . $site["siteTableName"] . "' ORDER BY LENGTH(`Text`) Limit ". $done - 100 .", ". $done ."");
+     //$done = 100 + $done;
+      $getflags = mysql_query("SELECT `Id` FROM flags WHERE handled=0 AND site='" . $site["siteTableName"] . "' ORDER BY LENGTH(`Text`) Limit 100");
+      echo "<span class='small'>MySQL part</span>";
       while ($flagid = mysql_fetch_assoc($getflags){
         array_push($result, $flagid["Id"]);
       }
@@ -35,5 +36,5 @@
         $timestamp = date("Y:m:d H:i:s");
         mysql_query("UPDATE flags SET handled=1, handleDate='$timestamp', wasValid=1, wasObsolete=1, handledBy= 0 WHERE `Id`=" . $valid. " AND  `site`='" . $site["siteTableName"] . "'");
       }
-  }
+  //}
   }
