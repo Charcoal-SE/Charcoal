030cdf35c91f7336a2d407e80178657392a40d44
hichris1234 <christopherncarmel@hotmail.com>
2014-01-07 11:58:10 -0500

    Update cronjob_2.php

--- cronjob_2.php
+++ cronjob_2.php
@@ -2,27 +2,38 @@
   //magic goes here
   include "base.php";
   
-  $query = mysql_query("SELECT Count(*) AS number, FROM flags WHERE handled=0 ORDER BY LENGTH(`Text`)");
-  $row = mysql_fetch_assoc($query);
-  $number = $row["number"];
-  print_r($number);
-  $done = 0;
-  while ($number - $done > 0)
+  $sites = mysql_query("SELECT * FROM sites");
+  while ($site = mysql_fetch_array($sites))
   {
-    $result = array();
-    $done = 100 + $done;
-    $getflags = mysql_query("SELECT `Id`, `site` FROM flags WHERE handled=0 ORDER BY LENGTH(`Text`) LIMIT 100");
-    while ($flagid = mysql_fetch_assoc($getflags){
-      array_push($result, $flagid["Id"]);
-    }
-    $result = implode(";",$result);
-    $url = 'https://api.stackexchange.com/2.1/posts/' . $result . '/comments';
-    $data = array("site" => $flagid["site"], "filter" => "!SrhZo6aE2O(w*j4-4i", "order" => "asc", key => "mmpZxopkL*psP5WoBK6BuA((");
-    $response = (new Curl)->exec($url . '?' . http_build_query($data), [CURLOPT_ENCODING => 'gzip']);
+    $query = mysql_query("SELECT Count(*) AS number, FROM flags WHERE handled=0 AND site='" . $site["siteTableName"] . "' ORDER BY LENGTH(`Text`)");
+    $row = mysql_fetch_assoc($query);
+    $number = $row["number"];
+    print_r($number);
+    $done = 0;
+    while ($number - $done > 0)
+    {
+      $result = array();
+      $done = 100 + $done;
+      $getflags = mysql_query("SELECT `Id`, `site` FROM flags WHERE handled=0 AND site='" . $site["siteTableName"] . "' ORDER BY LENGTH(`Text`) Limit ". $done - 100 .", ". $done ."");
+      while ($flagid = mysql_fetch_assoc($getflags){
+        array_push($result, $flagid["Id"]);
+      }
+      $result1 = implode(";",$result);
+      $url = 'https://api.stackexchange.com/2.1/posts/' . $result1 . '/comments';
+      $data = array("site" => $site["siteTableName"], "filter" => "!SrhZo6aE2O(w*j4-4i", "order" => "asc", key => "mmpZxopkL*psP5WoBK6BuA((");
+      $response = (new Curl)->exec($url . '?' . http_build_query($data), [CURLOPT_ENCODING => 'gzip']);
  
-    $obj1 = json_decode($response);
-    print_r($obj1);
-    $items = $obj1->{'items'};
-    print_r($items);
+      $obj1 = json_decode($response, true);
+      print_r($obj1);
+      //$items = $obj1->{'items'};
+      $comments = array_filter($obj1['items'], function ($a) {
+        return in_array($a['comment_id']);
+      });
+      print_r($comments);
+      $deleted = array_diff($result, $comments);
+      foreach ($deleted as $valid){
+        $timestamp = date("Y:m:d H:i:s");
+        mysql_query("UPDATE flags SET handled=1, handleDate='$timestamp', wasValid=1, wasObsolete=1, handledBy= 0 WHERE `Id`=" . $valid. " AND  `site`='" . $site["siteTableName"] . "'");
+      }
   }
-  
+  }
