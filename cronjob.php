3c0f47077496b1c81116bb3470f2a3fbb8fc6589
hichris1234 <christopherncarmel@hotmail.com>
2014-01-09 15:39:22 -0500

    Update cronjob.php

--- cronjob.php
+++ cronjob.php
@@ -5,13 +5,15 @@
 	$siteAPIKey = $siteArr['siteRootURL'];
 	$siteTableName = $siteArr["siteTableName"];
 
-	$query = mysql_query("SELECT `Id` FROM flags WHERE site='" . $siteTableName . "' AND handled=0 ORDER BY LENGTH(`Text`) limit 100");
+	//$query = mysql_query("SELECT `Id` FROM flags WHERE site='" . $siteTableName . "' AND handled=0 ORDER BY LENGTH(`Text`) limit 100");
+	$query = mysql_query("SELECT `Id` FROM flags WHERE site=stackoverflow AND handled=0 ORDER BY LENGTH(`Text`) limit 100");
 	$commentsToInspect = array();
 
 	while ($row = mysql_fetch_array($query))
 	{
 		$commentsToInspect[] = $row["Id"];
 	}
+	
 	print_r(count($commentsToInspect));
 	$url = 'https://api.stackexchange.com/2.1/comments/' . implode(";", $commentsToInspect);
 	$data = array("site" => $siteAPIKey, "filter" => "!9im-EfY_i", "order" => "asc", 'key' => "mmpZxopkL*psP5WoBK6BuA((");
