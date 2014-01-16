e8d5cd4848d6ac040749f2639a9993c1a0c7c91e
hichris1234 <christopherncarmel@hotmail.com>
2014-01-15 19:06:01 -0500

    Update cronjob.php

--- cronjob.php
+++ cronjob.php
@@ -48,5 +48,5 @@
 	
 	foreach ($commentsToInspect as $valid){
               $timestamp = date("Y:m:d H:i:s");
-              mysql_query("UPDATE flags SET handled=1, handleDate='$timestamp', wasValid=1, wasObsolete=1, handledBy=0 WHERE `Id`=" . $valid. " AND  `site`='" . $siteTableName  . "'");
+              mysql_query("UPDATE flags SET handled=1, handleDate='$timestamp', lastCronCheck='time', wasValid=1, wasObsolete=1, handledBy=0 WHERE `Id`=" . $valid. " AND  `site`='" . $siteTableName  . "'");
         }
