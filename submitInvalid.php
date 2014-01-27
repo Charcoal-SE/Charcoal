2a8e4e8202cadf92ce117e69c9a19fd4086d0cde
Undo1 <Undo1@users.noreply.github.com>
2014-01-26 17:33:49 -0700

    make way for posts

--- submitInvalid.php
+++ submitInvalid.php
@@ -1,8 +1,8 @@
 <?php
 	include "base.php";
 	$timestamp = date("Y:m:d H:i:s");
-
-	mysql_query("UPDATE flags SET handled=1, handleDate='$timestamp', wasValid=0, wasObsolete=0, handledBy=" . $_SESSION["UserID"] . " WHERE `Id`=" . $_POST['id']. " AND  `site`='" . $_SESSION["Site"] . "'");
+	
+	mysql_query("UPDATE " . (($_POST['isPost']==1) ? "postflags" : "flags") . " SET handled=1, handleDate='$timestamp', wasValid=0, wasObsolete=0, handledBy=" . $_SESSION["UserID"] . " WHERE `Id`=" . $_POST['id']. " AND  `site`='" . $_SESSION["Site"] . "'");
 
 	echo "success";
 ?>
