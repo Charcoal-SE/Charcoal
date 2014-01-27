7015b89d71ad43e4ca9e5212213b0eef0ecf80a7
Undo1 <Undo1@users.noreply.github.com>
2014-01-26 19:00:22 -0700

    Update posts.php

--- posts.php
+++ posts.php
@@ -44,10 +44,10 @@
             echo "<div class='postscore col-md-1'><h2 class='text-muted'>" . $row["Score"] . "</h2></div>";
             echo "<div class='postbody col-md-6 lead'><p>" . $row["Body"] . "</p></div>";
             echo "</div>";
-            echo "</td></tr>";
-            echo "<tr class='actions ". $row['Id'] . "' style='border-top:none'><td>";
+            echo "<div class='actions ". $row['Id'] . "' style='border-top:none'>";
             echo "<div class='btn btn-success valid-button col-md-offset-1' id='" . $row["Id"] . "' data-postid='".$row["Id"]."'  ><strong>valid</strong></div>";
             echo "<div class='btn btn-danger invalid-button' id='" . $row["Id"] . "' data-postid='".$row["Id"]."' style='margin-left:10px'><strong>invalid</strong></div>";
+            echo "</div>";
             echo "</td></tr>";
             
           }
